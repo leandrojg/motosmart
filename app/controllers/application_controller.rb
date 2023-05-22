b63904951917397fe16a639148b91acbe960033d
lean-planner <leandro.gordillo@plannereat.com>
2023-05-22 04:33:32 -0300

    added token verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    uuid = request.headers['UUID']
+    token = request.headers['Token']
+    
+    user = User.find_by(id: uuid, token: token)
+    
+    if user.nil?
+      render json: { error: 'Autenticación fallida' }, status: :unauthorized
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 require 'colorize'
 class UsersController < ApplicationController
-  #skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user, only: [:create]
 
   # GET /users
   def index
